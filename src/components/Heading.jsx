3e75e55430c10225b8e0367242bcb98314dd24f8
Kia <kivi0001@stud.kea.dk>
2026-03-24 11:58:47 +0100

    begyndt komponent arbejde, installeret icons

---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+const Heading = () => {
+  return (
+    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+      To get started, edit the page.js file.
+    </h1>
+  );
+};
+
+export default Heading;
